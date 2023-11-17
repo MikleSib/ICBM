b9d64f21fd08587d81b50581e8a7f6b659d482e9
MikleSib <Mishatrof21@gmail.com>
2023-11-17 11:37:32 +0300

    Добавлен захват региона

--- Assets/mapperClick.cs
+++ Assets/mapperClick.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WorldMapStrategyKit;
 
 public class mapperClick : MonoBehaviour
 {
-    // Start is called before the first frame update
+    WMSK map;
+    //Ru - 180
     void Start()
     {
-        
+         map = WMSK.instance;
+        map.OnProvinceClick += Map_OnProvinceClick;
+    }
+
+    private void Map_OnProvinceClick(int provinceIndex, int regionIndex, int buttonIndex)
+    {
+        map.CountryTransferProvince(180, provinceIndex, true);
     }
 
     // Update is called once per frame
